f2ce0035e2af0f114773619d1d6f42d4736082d8
KAREENA <KAREENAK@UDEL.EDU>
2023-02-21 16:36:01 -0500

    added starter code for second file

---
+++ first-exercises/forkprocesses.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int main() {
+    int status;
+    int pid = fork();
+    
+    return 0;
+}
